65d12e76aa6134cf7b7fbf779c0f2342976ec0b5
Nikko <nakolean@gmail.com>
2019-02-10 11:35:27 -0500

    Removing all boiler plate from the Gatsby starter

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,11 @@
 import React from "react"
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import { Typography } from "@material-ui/core";
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <React.Fragment>
+    <Typography variant='h1'>NOT FOUND</Typography>
+    <Typography variant='body1'>You just hit a route that doesn&#39;t exist... the sadness.</Typography>
+  </React.Fragment>
 )
 
 export default NotFoundPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
-
-import Layout from "../components/layout"
-import SEO from "../components/seo"
 import { Typography } from "@material-ui/core";
 
 const theme = createMuiTheme({
@@ -15,13 +12,7 @@ const theme = createMuiTheme({
 
 const IndexPage = () => (
   <MuiThemeProvider theme={theme}>
-    <Layout>
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      <Typography variant='h2'>Hi people</Typography>
-      <Typography variant='body1'>Welcome to your new Gatsby site.</Typography>
-      <Typography variant='body1'>Now go build something great.</Typography>
-      <Link to="/page-2/">Go to page 2</Link>
-    </Layout>
+    <Typography variant='h2'>Hi people</Typography>
   </MuiThemeProvider>
 )
 
