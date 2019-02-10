f8956b36cabce2b51c4cf58bfe19de86bb61459a
Nikko <nakolean@gmail.com>
2019-02-10 11:02:24 -0500

    Wrapping blog in default mui theme. Updating 'gatsby-config' to non default name and values

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Advice from an Unsuccessful`,
+    description: `Static blog using React and Material-UI.`,
+    author: `@nakolean@gmail.com`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `advice-from-an-unsuccessful`,
+        short_name: `unsuccessful-advice`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,29 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import { Typography } from "@material-ui/core";
+
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true,
+  },
+});
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  <MuiThemeProvider theme={theme}>
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      <Typography variant='h2'>Hi people</Typography>
+      <Typography variant='body1'>Welcome to your new Gatsby site.</Typography>
+      <Typography variant='body1'>Now go build something great.</Typography>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  </MuiThemeProvider>
 )
 
 export default IndexPage
