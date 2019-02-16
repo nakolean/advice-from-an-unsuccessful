2d36f5276d1fcd62982b4d6760dcf3b502f4e273
Nikko <nakolean@gmail.com>
2019-02-16 11:41:23 -0500

    Navigation is now using querying to get site title

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -5,36 +5,43 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
+// import IconButton from '@material-ui/core/IconButton'
+// import MenuIcon from '@material-ui/icons/Menu'
 
-const styles = {
+import { StaticQuery, graphql } from 'gatsby';
+
+const styles = theme => ({
   root: {
     flexGrow: 1,
   },
   grow: {
     flexGrow: 1,
   },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-};
+  // menuButton: {
+  //   marginLeft: `${theme.spacing.rem}rem`,
+  //   marginRight: `${theme.spacing.rem}rem`,
+  // },
+});
 
 const NavBar = ({ classes }) => (
-  <div className={classes.root}>
-    <AppBar>
-      <Toolbar>
-        <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" color="inherit" className={classes.grow}>
-          News
-        </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
-  </div>
+  <StaticQuery
+    query={detailsQuery}
+    render={data => (
+      <div className={classes.root}>
+        <AppBar>
+          <Toolbar>
+            {/* <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton> */}
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              {data.site.siteMetadata.title}
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )}
+    />
 );
 
 NavBar.propTypes = {
@@ -42,3 +49,15 @@ NavBar.propTypes = {
 };
 
 export default withStyles(styles)(NavBar);
+
+const detailsQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`
--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,16 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles'
+import indigo from '@material-ui/core/colors/indigo'
 
 const theme = createMuiTheme({
   typography: {
     useNextVariants: true,
   },
+  palette: {
+    primary: indigo
+  },
+  spacing: {
+    rem: 1
+  }
 })
 
 export default theme
