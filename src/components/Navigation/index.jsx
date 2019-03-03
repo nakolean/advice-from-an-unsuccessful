e1816961fb2227097e85c7b5c454b5c6c15341c3
Nikko <nakolean@gmail.com>
2019-03-03 10:46:37 -0500

    Adding Margin Bottom to Nav bar menu

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -1,33 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { StaticQuery, graphql } from 'gatsby';
 import { Divider } from '@material-ui/core';
 import Link from '@Navigation/Link';
 
-const styles = () => ({
+const styles = theme => ({
   root: {
     flex: 1,
     justifyContent: 'center',
   },
+  divider: {
+    marginBottom: theme.spacing.unit * 4,
+  }
 });
 
 const NavBar = ({ classes }) => (
   <StaticQuery
     query={detailsQuery}
     render={data => (
-      <div>
+      <Grid container item xs={12}>
         <Toolbar className={classes.root}>
-          <Typography variant="h5">
-            <Link to="/">
-              {data.site.siteMetadata.title}
-            </Link>
-          </Typography>
+          <Grid item>
+            <Typography variant="h5">
+              <Link to="/">
+                {data.site.siteMetadata.title}
+              </Link>
+            </Typography>
+          </Grid>
         </Toolbar>
-        <Divider />
-      </div>
+        <Grid item xs={12}>
+          <Divider className={classes.divider} />
+        </Grid>
+      </Grid>
     )}
     />
 );
